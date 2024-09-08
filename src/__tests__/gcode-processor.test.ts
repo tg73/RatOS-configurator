269d1c053e919ca29a5d9136b35a4973de809a9c
Tom Glastonbury <1101693+tg73@users.noreply.github.com>
2024-09-08 20:21:31 +0100

    first simple tests works, with callback-based disk writes.

--- src/__tests__/gcode-processor.test.ts
+++ src/__tests__/gcode-processor.test.ts
@@ -1,20 +1,26 @@
 /* eslint-disable no-console */
+import { createReadStream } from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import { describe, expect, test, bench } from 'vitest';
-
+import split from 'split2';
+import { stdout } from 'process';
+import { pipeline } from 'node:stream/promises';
+import { BookmarkingWriter, SlidingWindowLineProcessor } from '@/server/gcode-processor/StreamingLineProcessor';
 
 describe('gcode-processor', (async) => {
 	test('foo99', async () => {
 		console.log('In foo99');
 		const name = path.join(__dirname, 'fixtures', 'v-core-200.json');
-		//const file = createReadStream(name);
-		//const toLines = split();
-		//let idx = 0;
-		//toLines.on('data', function (line) {
-		//	console.log('%d: %s', ++idx, line);
-		//});
-
-		//let ll = <string[]>[];
-		//await pipeline(file, toLines);
+		let fh = await fs.open('/home/tom/temp/test.out', 'w');
+		await pipeline(
+			createReadStream(name),
+			split(),
+			new SlidingWindowLineProcessor((line, getNearbyLine) => {
+				console.log(line);
+				return line;
+			}),
+			new BookmarkingWriter(fh.fd),
+		);
 	});
 });
--- src/server/gcode-processor/StreamingLineProcessor.ts
+++ src/server/gcode-processor/StreamingLineProcessor.ts
@@ -1,3 +1,16 @@
+/* eslint-disable no-console */
+/**
+ * @file StreamingLineProcessor.ts
+ * @description
+ * TODO
+ *
+ * @author Tom Glastonbury <t@tg73.net>
+ * @license MIT
+ * @copyright 2024
+ */
+
+// TODO: Exception handling!
+
 /*
 https://www.paigeniedringhaus.com/blog/streams-for-the-win-a-performance-comparison-of-node-js-methods-for-reading-large-datasets-pt-2
 
@@ -7,6 +20,7 @@ It concludes that only event-stream can handle multi-gb files. event-stream was 
 */
 
 import { createReadStream, fstat } from 'fs';
+import * as fssync from 'node:fs';
 import { open, FileHandle } from 'fs/promises';
 import split from 'split2';
 import path from 'path';
@@ -16,13 +30,9 @@ import { Transform, TransformCallback, TransformOptions, Writable } from 'node:s
 import { never } from 'zod';
 import { Deferred } from '@/server/gcode-processor/Deferred';
 import { RingBuffer } from 'ring-buffer-ts';
+import { of } from 'rxjs';
 
-interface Window {
-	// Gets a line before (-ve offset) or after (+ve offset) the current line being processed.
-	getLine(offset: number): string;
-}
-
-class Bookmark {
+export class Bookmark {
 	constructor(
 		public readonly byteOffset: number,
 		public readonly byteLength: number,
@@ -36,14 +46,14 @@ class Bookmark {
  *    the original line, so `RewritableLine.line` should be padded to allow for any extra space that may
  *    be required.
  */
-class BookmarkedLine {
+export class BookmarkedLine {
 	constructor(line: string) {
 		this.line = line;
-		this.writtenLine = new Deferred<Bookmark>();
+		this.bookmark = new Deferred<Bookmark>();
 	}
 
 	readonly line: string;
-	readonly writtenLine: Deferred<Bookmark>;
+	readonly bookmark: Deferred<Bookmark>;
 
 	toString() {
 		return this.line;
@@ -52,6 +62,9 @@ class BookmarkedLine {
 
 /**
  * Performs analysis and optionally mutation of a line.
+ * @param line The line to process.
+ * @param getNearbyLine A callback that obtains a nearby line. `null` indicates that a preceeding line has
+ *   been removed. `undefined` indicates that the given `offset` is outside the available window.
  *
  * The callback must return one of:
  * * `string`: the original line, or a different `string` to replace the original line
@@ -63,27 +76,61 @@ class BookmarkedLine {
  *     the original line, so `RewritableLine.line` should be padded to allow for any extra space that may
  *     be required.
  */
-type ProcessLineCallback = (
+export type ProcessLineCallback = (
 	line: string,
-	getNearbyLine: (offset: number) => string | null,
+	getNearbyLine: (offset: number) => string | null | undefined,
 ) => string | BookmarkedLine | null;
 
-class SlidingWindowLineProcessor extends Transform {
+export class SlidingWindowLineProcessor extends Transform {
 	constructor(
 		private callback: ProcessLineCallback,
 		public readonly maxLinesAhead = 10,
 		public readonly maxLinesBehind = 10,
 	) {
 		super({ objectMode: true });
-		this.#buf = new RingBuffer<string>(maxLinesAhead + maxLinesBehind + 1);
+		this.#buf = new RingBuffer<string>(maxLinesBehind + maxLinesAhead + 1);
+		//this.on('drain', () => console.log(">> DRAIN"));
+		//this.on('pause', () => console.log(">> PAUSE"));
+		//this.on('resume', () => console.log(">> RESUME"));
 	}
 
-	#position = 0;
-
-	/*
-	 * Holds circular buffer
+	/**
+	 * The current position within `#buf`. When the window is primed and streaming
+	 * is well underway, `#position` will be `maxLinesBehind`. During initial priming,
+	 * `#position` can be less than `maxLinesBehind`, and while processing the end of
+	 * the stream in `_flush`, `#position` can be greater than `maxLinesBehind`.
+	 * Other than during callback exectuion, the line at `#position` has already been
+	 * processed.
 	 */
+	#position = -1;
+
 	#buf: RingBuffer<string>;
+
+	#getNearbyLine(offset: number): string | null | undefined {
+		let p = this.#position + offset;
+		if (p < 0 || p >= this.#buf.getBufferLength()) {
+			return undefined;
+		}
+		return this.#buf.get(p);
+	}
+
+	#processExtraLines(action: 'prime' | 'flush', tfCallback: TransformCallback): void {
+		let limit = action === 'prime' ? this.maxLinesBehind : this.#buf.getBufferLength() - 1;
+
+		while (this.#position < limit) {
+			++this.#position;
+			let result = this.callback(this.#buf.get(this.#position)!, this.#getNearbyLine);
+			if (result !== null) {
+				if (!this.push(result)) {
+					this.once('resume', () => {
+						this.#processExtraLines(action, tfCallback);
+					});
+					return;
+				}
+			}
+		}
+		tfCallback();
+	}
 
 	_transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
 		// Populate buffer (or end):
@@ -91,44 +138,146 @@ class SlidingWindowLineProcessor extends Transform {
 		// process lines up to bias (centre) pos
 		// for (i 0 to 5) this.callback( this._buffer[i], (offset: number) => xxx )
 		// callback()
+
+		if (typeof chunk !== 'string') {
+			callback(new Error('chunk must be a string'));
+		}
+
+		if (!this.#buf.isFull()) {
+			// Not fully primed yet.
+			this.#buf.add(chunk);
+
+			if (!this.#buf.isFull()) {
+				return callback();
+			}
+		}
+
+		if (this.#position == -1) {
+			// Priming of the ring buffer has just completed. Process all lines up to position `maxLinesBehind`.
+			return this.#processExtraLines('prime', callback);
+		}
+
+		if (this.#position != this.maxLinesBehind) {
+			throw new Error('Unexpected state!');
+		}
+
+		this.#buf.add(chunk);
+		let result = this.callback(this.#buf.get(this.#position)!, this.#getNearbyLine);
+
+		if (result !== null) {
+			if (!this.push(result)) {
+				this.once('resume', () => callback());
+				return;
+			}
+		}
+
+		return callback();
 	}
 
-	_flush(callback: TransformCallback): void {}
+	_flush(callback: TransformCallback): void {
+		// eslint-disable-next-line no-console
+		//console.log(`FLUSH: pos=${this.#position} glb=${this.#buf.getBufferLength()} size=${this.#buf.getSize()}`);
+		return this.#processExtraLines('flush', callback);
+	}
 }
 
-// TODO: OutputSink should have only the members of FileHandle that we need for
-// writing ops, then a buffer-backed mock can be developed.
-interface OutputSink {}
+/**
+ * `BookmarkingWriterDestination` contains the methods of `FileHandle` required by `BookmarkingWriter` and
+ * `replaceBookmarkedGcodeLine`. This allows other implementations to be used, for example for testing without
+ * using disk writes.
+ */
+export interface BookmarkingWriterFileHandle {
+	write(
+		data: string,
+		position?: number | null,
+		encoding?: BufferEncoding | null,
+	): Promise<{
+		bytesWritten: number;
+		buffer: string;
+	}>;
 
-class BookmarkingWriter extends Writable {
-	constructor(private fh: FileHandle) {
+	write<TBuffer extends Uint8Array>(
+		buffer: TBuffer,
+		offset?: number | null,
+		length?: number | null,
+		position?: number | null,
+	): Promise<{
+		bytesWritten: number;
+		buffer: TBuffer;
+	}>;
+}
+
+export class BookmarkingWriter extends Writable {
+	constructor(
+		private readonly fd: number, //BookmarkingWriterFileHandle,
+		public readonly encoding: BufferEncoding = 'utf8',
+	) {
 		super({ objectMode: true });
 	}
 
 	#bytesWritten: number = 0;
 
+	// https://github.com/nodejs/node/issues/31387
+
 	_write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
-		if (chunk === null) {
-			return callback();
-		} else if (chunk instanceof BookmarkedLine) {
-			this.fh.write(chunk.line).then(
-				(result) => {
-					chunk.writtenLine.value = new Bookmark(this.#bytesWritten, result.bytesWritten);
-					this.#bytesWritten += result.bytesWritten;
+		try {
+			if (chunk === null) {
+				return callback();
+			} else if (chunk instanceof BookmarkedLine) {
+				/*
+				this.fh.write(chunk.line, null, this.encoding).then(
+					(result) => {
+						chunk.bookmark.value = new Bookmark(this.#bytesWritten, result.bytesWritten);
+						this.#bytesWritten += result.bytesWritten;
+						return callback();
+					},
+					(reason) => callback(new Error(reason)),
+				);
+				*/
+			} else if (typeof chunk === 'string') {
+				//console.log('.');
+				//return callback();
+				fssync.write(this.fd, chunk, (err, bytesWritten) => {
+					if (err) {
+						return callback(err);
+					}
+					this.#bytesWritten += bytesWritten;
 					return callback();
-				},
-				(reason) => callback(new Error(reason)),
-			);
-		} else if (typeof chunk === 'string') {
-			this.fh.write(chunk).then(
-				(result) => {
-					this.#bytesWritten += result.bytesWritten;
-					return callback();
-				},
-				(reason) => callback(new Error(reason)),
-			);
-		} else {
-			callback(new Error('Unexpected type!'));
+				});
+				return;
+			} else {
+				return callback(new Error('Unexpected type!'));
+			}
+		} catch (err) {
+			return callback(err instanceof Error ? err : new Error('Unknown error.'));
 		}
 	}
+}
+
+/**
+ * Replaces a bookmarked line in a file. GCode-compatible padding is added as required to match the size of the line being
+ * replaced. The file must be UTF8 encoded.
+ * @param fh
+ * @param bookmark
+ * @param replacementLine
+ * @param encoding
+ */
+export async function replaceBookmarkedGcodeLine(
+	fh: FileHandle,
+	bookmark: Bookmark,
+	replacementLine: string,
+): Promise<void> {
+	let line = replacementLine.trimEnd();
+	let buf = Buffer.from(line);
+	if (buf.length + 1 > bookmark.byteLength) {
+		// Too long, allowing for terminating \n.
+		throw new RangeError(
+			`The line cannot be replaced in-place. The replacement requires ${buf.length + 1} bytes, but only ${bookmark.byteLength} bytes are available.`,
+		);
+	}
+	buf = Buffer.from(line.padEnd(buf.length - bookmark.byteLength - 1) + '\n');
+	if (buf.length != bookmark.byteLength) {
+		throw new Error('Unexpected length mismatch!');
+	}
+	await fh.write(buf, bookmark.byteOffset);
 }
