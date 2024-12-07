ac4ca581c52ec91ae337249daff52a0d3626f906
Mikkel Schmidt <msc@orbit.online>
2024-12-08 00:31:11 +0100

    Scripts: fix paths and missing SCRIPT_DIR

--- configuration/scripts/generate-shaper-graph-y.sh
+++ configuration/scripts/generate-shaper-graph-y.sh
@@ -7,7 +7,7 @@ source "$SCRIPT_DIR"/ratos-common.sh
 
 NEWY=$(find /tmp -name "resonances_y_*.csv" -printf '%T@ %p\n' 2> /dev/null | sort -n | tail -1 | cut -f2- -d" ")
 DATE=$(date +'%Y-%m-%d-%H%M%S')
-outdir="${SCRIPT_DIR}"/../../input_shaper
+outdir="${RATOS_PRINTER_DATA_DIR}"/config/input_shaper
 if [ ! -d "${outdir}" ]
 then
     mkdir "${outdir}"
