7002ca66974e2d365eef43deb16bb6b9a628c18b
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2024-12-20 00:28:30 +0000

    Deployed 1 commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/bd98ecac76e98c19be1255bfb441e6cf59669d41...3ab53da1c873760fe54a98456d2c230bb86d8f8f

--- app/scripts/update.sh
+++ app/scripts/update.sh
@@ -31,6 +31,8 @@ ensure_service_permission
 ensure_sudo_command_whitelisting
 ensure_pnpm_installation
 pnpm_install
+ensure_pnpm_setup
+install_global_pnpm_packages
 install_cli
 install_udev_rule
 patch_log_rotation
