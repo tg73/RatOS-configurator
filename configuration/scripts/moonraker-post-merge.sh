068fb16b2eba1d841aa03919b523b06acd2a4a2a
tg73 <tg73@users.noreply.github.com>
2025-07-08 15:06:30 +0000

    Deployed 0 commit. See the complete list of changes here: https://github.com/tg73/RatOS-configurator/compare/559036290158dd8a7b47b2029c853da81d5830e5...559036290158dd8a7b47b2029c853da81d5830e5

---
+++ configuration/scripts/moonraker-post-merge.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
+# shellcheck source=./configuration/scripts/moonraker-update.sh
+sudo "$SCRIPT_DIR"/moonraker-update.sh
