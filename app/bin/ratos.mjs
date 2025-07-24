cd204470ea5688fbef7c4c4efeda931ecc4ede02
tg73 <tg73@users.noreply.github.com>
2025-07-24 14:22:38 +0000

    Deployed 3 commits. See the complete list of changes here: https://github.com/tg73/RatOS-configurator/compare/bb5ac679a0d6e697ac19853d9ca9d2e5afdce989...0a645c467c3392ff3839199873d5fb692f264293

--- app/bin/ratos.mjs
+++ app/bin/ratos.mjs
@@ -101949,9 +101949,9 @@ function validateGenerator(gcodeInfo, allowUnsupportedSlicerVersions, onWarning)
           { cause: gcodeInfo }
         );
       case 1 /* PrusaSlicer */:
-        if (!import_semver2.default.satisfies(gcodeInfo.generatorVersion, "2.8.0 || 2.8.1 || 2.9.0")) {
+        if (!import_semver2.default.satisfies(gcodeInfo.generatorVersion, "2.8.0 || 2.8.1 || 2.9.0 || 2.9.1 || 2.9.2")) {
           throw new SlicerNotSupported(
-            `Only versions 2.8.0, 2.8.1 and 2.9.0 of PrusaSlicer are supported. Version ${gcodeInfo.generatorVersion} is not supported.`,
+            `Only release versions 2.8.0, 2.8.1 and 2.9.0 - 2.9.2 of PrusaSlicer are supported. Version ${gcodeInfo.generatorVersion} is not supported.`,
             { cause: gcodeInfo }
           );
         }
