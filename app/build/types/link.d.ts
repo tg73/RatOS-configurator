ef47734616f289f7f51884209421d8a560cd4e25
tg73 <tg73@users.noreply.github.com>
2025-07-25 13:28:02 +0000

    Deployed 1 commit. See the complete list of changes here: https://github.com/tg73/RatOS-configurator/compare/25ef320ac76b99e0ae51da3975e63032e7a1045c...4f0df534160d4420f582ac8e096b255d302a51df

--- app/build/types/link.d.ts
+++ app/build/types/link.d.ts
@@ -36,20 +36,20 @@ declare namespace __next_route_internal_types__ {
     | `/api/update-logs/download`
     | `/api/update-logs/generate-test-data`
     | `/`
-    | `/motion`
     | `/calibration`
+    | `/motion`
     | `/toolhead`
-    | `/wizard`
     | `/update-logs`
-    | `/analysis`
+    | `/wizard`
     | `/analysis/macros/new`
+    | `/analysis`
     | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
