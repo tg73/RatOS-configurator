aca86a3c0cb1e963fc92c2094588af4c1baf864c
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2025-11-11 18:45:47 +0000

    Deployed  commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/...1ab46dc36256a4c4e06a221511df3a9de6f0e148

--- app/build/types/link.d.ts
+++ app/build/types/link.d.ts
@@ -37,16 +37,16 @@ declare namespace __next_route_internal_types__ {
     | `/calibration`
     | `/motion`
     | `/toolhead`
+    | `/analysis`
     | `/wizard`
     | `/analysis/macros/new`
     | `/analysis/macros`
-    | `/analysis`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
+    | `/analysis/macros/${SafeSlug<T>}`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
