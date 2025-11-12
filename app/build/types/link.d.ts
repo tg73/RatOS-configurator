1fc92d43968b52420a20300b89413c86a94cb1b6
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2025-11-12 13:10:17 +0000

    Deployed  commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/...1ab46dc36256a4c4e06a221511df3a9de6f0e148

--- app/build/types/link.d.ts
+++ app/build/types/link.d.ts
@@ -31,16 +31,16 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/dfu-image`
     | `/api/download-firmware`
-    | `/api/printer-image`
     | `/api/mcu-image`
+    | `/api/printer-image`
     | `/`
-    | `/motion`
     | `/calibration`
+    | `/motion`
     | `/toolhead`
-    | `/analysis/macros/new`
     | `/wizard`
-    | `/analysis`
+    | `/analysis/macros/new`
     | `/analysis/macros`
+    | `/analysis`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
