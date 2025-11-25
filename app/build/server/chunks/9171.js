cafe85df00f631349ad206fde7df34d612e8dd13
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2025-11-25 15:18:55 +0000

    Deployed  commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/...1ab46dc36256a4c4e06a221511df3a9de6f0e148

--- app/build/server/chunks/9171.js
+++ app/build/server/chunks/9171.js
@@ -1,8 +1,8 @@
-"use strict";exports.id=9171,exports.ids=[9171],exports.modules={9171:(e,t,n)=>{n.r(t),n.d(t,{getRequiredPinAliases:()=>getRequiredPinAliases,renderTemplate:()=>renderTemplate});var o=n(12051);let r=o.z.object({invertRunoutPin:o.z.boolean().default(!1),pullUpRunoutPin:o.z.boolean().default(!0)}),getRequiredPinAliases=e=>["filament_sensor_runout_pin"],renderTemplate=e=>{let t=e.toolheadGenerator,n=r.parse(e.templateOptions??{});return`
+"use strict";exports.id=9171,exports.ids=[9171],exports.modules={9171:(e,t,o)=>{o.r(t),o.d(t,{getRequiredPinAliases:()=>getRequiredPinAliases,renderToolheadTemplate:()=>renderToolheadTemplate});var n=o(12051);let l=n.z.object({invertRunoutPin:n.z.boolean().default(!1),pullUpRunoutPin:n.z.boolean().default(!0)}),getRequiredPinAliases=e=>["filament_sensor_runout_pin"],renderToolheadTemplate=e=>{let t=e.utils.getToolhead(e.toolNumber),o=l.parse(e.templateOptions??{});return`
 [filament_switch_sensor filament_sensor${t.printerHasMultipleToolheads?`_${t.getShortToolName()}`:""}]
 pause_on_runout: False
 event_delay: 1.0
-switch_pin: ${n.invertRunoutPin?"!":""}${n.pullUpRunoutPin?"^":""}${t.getPinPrefix()}${t.getPinFromAlias("filament_sensor_runout_pin")}
+switch_pin: ${o.invertRunoutPin?"!":""}${o.pullUpRunoutPin?"^":""}${e.getPrefixedPinFromAlias("filament_sensor_runout_pin")}
 runout_gcode:
 	_ON_TOOLHEAD_FILAMENT_SENSOR_RUNOUT TOOLHEAD=${t.getTool()}
 insert_gcode:
--- app/build/types/link.d.ts
+++ app/build/types/link.d.ts
@@ -36,16 +36,16 @@ declare namespace __next_route_internal_types__ {
     | `/`
     | `/calibration`
     | `/motion`
-    | `/toolhead`
     | `/wizard`
-    | `/analysis/macros`
+    | `/toolhead`
     | `/analysis/macros/new`
     | `/analysis`
+    | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
+    | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
 
   type RouteImpl<T> = 
