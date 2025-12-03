cb3d2a6a4e9e33e522955b699907053a2a38b6cf
tg73 <tg73@users.noreply.github.com>
2025-12-03 18:21:53 +0000

    Deployed 2 commits. See the complete list of changes here: https://github.com/tg73/RatOS-configurator/compare/a08419325670b73e8dc1129b943b5da63057d87d...19a07c13dd3483513c1288c1573cd71702fdf24b

--- app/build/types/link.d.ts
+++ app/build/types/link.d.ts
@@ -31,19 +31,19 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/dfu-image`
     | `/api/download-firmware`
-    | `/api/printer-image`
     | `/api/mcu-image`
+    | `/api/printer-image`
     | `/api/update-logs/download`
     | `/api/update-logs/generate-test-data`
     | `/`
     | `/calibration`
-    | `/toolhead`
     | `/motion`
+    | `/toolhead`
     | `/wizard`
+    | `/update-logs`
+    | `/analysis/macros`
     | `/analysis/macros/new`
     | `/analysis`
-    | `/analysis/macros`
-    | `/update-logs`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
